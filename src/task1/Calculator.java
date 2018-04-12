289b69542d24c61ea5fd29f470218fa99cb4b022
DmitryGM <erropp@ya.ru>
2018-04-12 23:41:44 +0300

    add task 1

---
+++ src/task1/Calculator.java
@@ -0,0 +1,45 @@
+package task1;
+
+public class Calculator {
+
+    private int getSign(int n) {
+        return 0 == n % 2 ? 1 : -1;
+    }
+
+    private double calculateSin(double x) {
+        assert 0 <= x && x < Math.PI/2;
+
+        double sum = 0;
+        int n = 15;
+
+        int fac = 1;
+
+        for (int i = 1; i < n; ++i) {
+            fac *= i;
+            if (0 == i%2)
+                continue;
+
+            sum += getSign(i/2) * Math.pow(x, i) / fac;
+        }
+
+        return sum;
+    }
+
+    public double getSin(double x) {
+        int sign = 1;
+        if (x < 0) {
+            x *= -1;
+            sign = -1;
+        }
+        x %= 2*Math.PI;
+        int k = (int)(2*x / Math.PI);
+        //double kkk = (2*x / Math.PI);
+        x %= Math.PI;
+
+        if (0 != k % 2) {
+            k--;
+            x = Math.PI - x;
+        }
+        return sign * getSign(k/2) * calculateSin(x);
+    }
+}
---
+++ src/task1/CalculatorTests.java
@@ -0,0 +1,41 @@
+package task1;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class CalculatorTests {
+
+    private final double eps = 10e-6;
+
+    @Test
+    public void integers() {
+        Calculator calc = new Calculator();
+        for (int i = -1000; i < 1000; ++i) {
+            double u = (double) i;
+            assertTrue(Math.abs(Math.sin(u) - calc.getSin(u)) < eps, "u = "+ u);
+        }
+    }
+
+    @Test
+    public void extremes() {
+        Calculator calc = new Calculator();
+        for (int i = -1000; i < 1000; ++i) {
+            double u = (double) i*(Math.PI/2);
+            assertTrue(Math.abs(Math.sin(u) - calc.getSin(u)) < eps, "u = "+ u + "; delta = " + Math.abs(Math.sin(u) - calc.getSin(u)));
+        }
+    }
+
+    @Test
+    public void random() {
+        Calculator calc = new Calculator();
+        Random rnd = new Random();
+        for (int i = -1000; i < 1000; ++i) {
+            double u = (double) i*rnd.nextDouble();
+            assertTrue(Math.abs(Math.sin(u) - calc.getSin(u)) < eps, "u = "+ u + "; delta = " + Math.abs(Math.sin(u) - calc.getSin(u)));
+        }
+    }
+
+
+}
