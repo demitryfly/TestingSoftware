73c7b6868f344b6fb1c85607ed9b6779465d2581
DmitryGM <erropp@ya.ru>
2018-04-20 03:48:09 +0300

    added Logging

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,19 @@ import task3.Atmosphere;
 import task3.Person;
 import task3.Rocket;
 import task3.Screen;
+import task4.calculator.Function;
+import task4.calculator.Logging;
+import task4.calculator.logarithmic.FunLog;
+import task4.calculator.logarithmic.Log;
+import task4.calculator.logarithmic.Log2;
+import task4.calculator.trigonometric.*;
 
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
+        /*
         Atmosphere atm = new Atmosphere();
         atm.addItem(new Rocket());
         atm.addItem(new Rocket());
@@ -33,8 +40,15 @@ public class Main {
         while (!ford.tryShow(screens.get("back"))) {
             ford.getShoulder().put();
         }
+        */
+
+        FunTrg funTrg = new FunTrg(new Tan(new Sin(), new Cos(new Sin())), new Sec(new Cos(new Sin())), new Csc(new Sin()));
+        FunLog funLog = new FunLog(new Log(), new Log2(new Log()));
+
+        Function fun = new Function(funLog, funTrg);
 
 
+        Logging.writeLogFile(fun, -10, 10, 0.1, 1);
 
 
 
